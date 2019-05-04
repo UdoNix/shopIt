5a514fb6c0a4eb8a54c475fe8ddf6bf62c93ffb1
Alex <Alex@PC-Alex>
2019-05-04 02:02:57 +0200

    Anpassung der EntryPoint (Test) - Programm kann ausgeführt werden

--- shopIt/src/de/hdm/client/ShopIt.java
+++ shopIt/src/de/hdm/client/ShopIt.java
@@ -1,6 +1,10 @@
 package de.hdm.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -11,6 +15,18 @@ public class ShopIt implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
+		/**
+		 * The Vertical Panel is a gwt standard, it content vertically
+		 */
+		VerticalPanel vPanel = new VerticalPanel();
 		
+		Label myLbl = new Label("Hallo");
+		
+		vPanel.add(myLbl);
+		
+		Button btn = new Button("Press");
+		vPanel.add(btn);
+		
+		RootPanel.get().add(vPanel);
 	}
 }
