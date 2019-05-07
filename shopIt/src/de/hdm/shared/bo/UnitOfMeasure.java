cde891612fdb74746126f1a25fadd494f94aeda0
inesw <inesw@10.60.35.243>
2019-05-07 10:09:04 +0200

    ((UnitOfMeasure) Attribute: unit, quantity; Methoden: getter und setter

---
+++ shopIt/src/de/hdm/shared/bo/UnitOfMeasure.java
@@ -0,0 +1,29 @@
+package de.hdm.shared.bo;
+public class UnitOfMeasure{
+
+	//Menge/Anzahl 
+	private float quantity;
+	
+	//Einheit
+	private String unit;
+
+	//Auslesen der Menge/Anzahl eines Produkts eines Eintrags.
+	public float getQuantity() {
+		return quantity;
+	}
+	//Setzen der Menge/Anzahl eines Produkts eines Eintrags (Bsp.: 1.5)
+	public void setQuantity(float quantity) {
+		this.quantity = quantity;
+	}
+	//Auslesen der Einheit eines Produkts eines Eintrags 
+	public String getUnit() {
+		return unit;
+	}
+	//Setzen der Einheit eines Produkts eines Eintrags (Bsp.: Liter, Palette)
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+
+}
+
+
