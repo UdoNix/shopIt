c4366848e399b7fb947ab08bcb571ade952bb010
Alex <Alex@PC-Alex>
2019-05-13 11:34:59 +0200

    Create TreeViewModel

--- shopIt/src/de/hdm/client/gui/CellTreeViewModel.java
+++ shopIt/src/de/hdm/client/gui/CellTreeViewModel.java
@@ -1,5 +1,8 @@
 package de.hdm.client.gui;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.cellview.client.CellTree;
+import com.google.gwt.user.client.ui.StackPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.view.client.TreeViewModel;
 
@@ -8,12 +11,8 @@ public class CellTreeViewModel {
 	
 	private static class createTreeModel extends VerticalPanel {
 		
-		private VerticalPanel menuPanel = new VerticalPanel();
+		private StackPanel menuPanel = new StackPanel(); 
 		
-		public createTreeModel() {
-			
-			
-		}
-
+		
 	}
 }
