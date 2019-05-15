85bf217eb708d7065dd1170142eed50c02eb893c
ilona <ilona@10.60.36.235>
2019-05-15 16:15:53 +0200

    List: Attribut Gruppe hinzugefügt + verändert

--- shopIt/src/de/hdm/shared/bo/List.java
+++ shopIt/src/de/hdm/shared/bo/List.java
@@ -44,7 +44,7 @@ public class List extends BusinessObject{
 	}
 	//Auslesen der Gruppe
 	public Group getGroup() {
-		return group;
+		return groupid;
 	}
 	//Setzen der Gruppe
 	public void setGroup(Group group) {
