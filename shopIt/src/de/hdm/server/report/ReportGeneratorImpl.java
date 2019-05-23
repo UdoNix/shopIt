666199e7e5b85d6c03e4766fe80cb4d02297e184
InesWerner <iw023@hdm-stuttgart.de>
2019-05-23 17:25:33 +0200

    Klasse ReportGeneratorImpl angelegt

---
+++ shopIt/src/de/hdm/server/report/ReportGeneratorImpl.java
@@ -0,0 +1,7 @@
+package de.hdm.server.report;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+public class ReportGeneratorImpl extends RemoteServiceServlet implements ReportGenerator{
+
+
+}
