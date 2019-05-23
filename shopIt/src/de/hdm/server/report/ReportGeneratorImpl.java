f56a5296d639330e3abaa692fe23fb16d5635135
InesWerner <iw023@hdm-stuttgart.de>
2019-05-23 18:09:25 +0200

    Test: Hinzufügen @SuppressWarnings("serial")

--- shopIt/src/de/hdm/server/report/ReportGeneratorImpl.java
+++ shopIt/src/de/hdm/server/report/ReportGeneratorImpl.java
@@ -1,6 +1,9 @@
 package de.hdm.server.report;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
+import de.hdm.shared.ReportGenerator;
+
+@SuppressWarnings("serial")//Unterdrückung von Warnungen bezüglich fehlendem Feld 'serialVersionUID' für eine serialisierbare Klasse
 public class ReportGeneratorImpl extends RemoteServiceServlet implements ReportGenerator{
 
 
