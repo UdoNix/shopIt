1c469f587422c95c217ac4d9aa657b8508e07cb3
IlonaBrinkmann <ib036@hdm-stuttgart.de>
2019-05-24 20:02:30 +0200

    Änderung AllItemsOfPersonReport Änderung

--- shopIt/src/de/hdm/shared/report/AllItemsOfPersonReport.java
+++ shopIt/src/de/hdm/shared/report/AllItemsOfPersonReport.java
@@ -1,5 +1,11 @@
 package de.hdm.shared.report;
 
-public class AllItemsOfPersonReport {
+public class AllItemsOfPersonReport extends SimpleReport{
 
+	/*
+	 * Report der alle Konten eines Kunden darstellt
+	 * @author Thies Ilona
+	 */
+	private static final long serialVersionUID = 1L;
+	
 }
