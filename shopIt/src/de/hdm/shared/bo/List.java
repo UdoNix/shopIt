f68c41112ac58dd77646b0d7b96a7e5624ed737a
InesWerner <iw023@hdm-stuttgart.de>
2019-05-31 10:47:08 +0200

    kleine Anpassung List: int groupId

--- shopIt/src/de/hdm/shared/bo/List.java
+++ shopIt/src/de/hdm/shared/bo/List.java
@@ -1,20 +1,16 @@
 package de.hdm.shared.bo;
-import java.sql.Date;
+
 
 public class List extends BusinessObject{
 
 	private static final long serialVersionUID = 1L;
 
-	private Item item;
 	
 	//Name der Einkaufsliste.
 	private String name = "";
-	
-	//Änderungsdatum der letzten Veränderung der Liste.
-	//private Date creationDate;
 
 	//Fremdschlüssel zur Gruppe.
-	private Group groupId;
+	private int groupId;
 
 	//Auslesen des Listennamen.
 	public String getName() {
@@ -25,35 +21,17 @@ public class List extends BusinessObject{
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	//Auslesen des Erstellungsdatum.
-	//public Date getCreationDate() {
-	//	return creationDate;
-	//}
-	//Setzen des Erstellungsdatum.
-	//public void setCreationDate(Date creationDate) {
-	//	this.creationDate = creationDate;
-	//}
-	
-	//Auslesen des Fremdschlüssels zur GruppenId.
-	public Group getGroupId() {
+
+	//Auslesen der Fremdschlüsselbeziehung zur Gruppe.
+	public int getGroupId() {
 		return groupId;
 	}
-	
-	//Setzen des Fremdschlüssels zur GruppenId.
-	public void setGroupId(Group groupId) {
+
+	//Setzen der Fremdschlüsselbeziehung zur Gruppe.
+	public void setGroupId(int groupId) {
 		this.groupId = groupId;
 	}
-	
-	//Auslesen des Listeneintrags.
-	public Item getItem() {
-		return item;
-	}
-	
-	//Setzen eines Listeneintrags.
-	public void setItem(Item item) {
-		this.item = item;
-	}
+
 
 
 }
