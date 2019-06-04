f1bdf2bab45973db6383250e53827611ddf65ac9
dibasegmen <ds173@hdm-stuttgart.de>
2019-06-04 21:56:48 +0200

    Erstellen einer Klasse, die das Verlassen einer Gruppe ermöglicht.

---
+++ shopIt/src/de/hdm/client/gui/LeaveGroupDialogBox.java
@@ -0,0 +1,78 @@
+package de.hdm.client.gui;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+import de.hdm.client.ClientsideSettings;
+import de.hdm.shared.EditorService;
+import de.hdm.shared.EditorServiceAsync;
+import de.hdm.shared.bo.*;
+
+/*
+ * Die Klasse <code>LeaveGroupDialogBox</code> dient der Darstellung einer Dialogbox, die
+ * dem Nutzer das Verlassen einer Gruppe ermöglicht
+ * 
+ * @author dibasegmen
+ *
+*/
+
+public class LeaveGroupDialogBox extends DialogBox {
+	
+	//Entsprechendes Interface wird noch angepasst
+	
+	private EditorServiceAsync listenverwaltung = ClientsideSettings.getEditorService();
+	
+	private Person p = currentPerson.getPerson();
+	private Group selectedGroup = null;
+	
+	private VerticalPanel contentPanel = new VerticalPanel();
+	private HorizontalPanel buttonPanel = new HorizontalPanel();
+	
+	private Button confirmBtn = new Button ("Gruppe verlassen");
+	private Button cancelBtn = new Button("Abbrechen");
+	private Label confirmLabel = new Label ("Möchten Sie die Gruppe wirklich verlassen?");
+
+	
+	public LeaveGroupDialogBox(){
+		
+		//Aktivierung des Glass Backgrounds einer DialogBox
+		
+		this.setGlassEnabled(true);
+		
+		cancelBtn.setStylePrimaryName("cancelButton");
+		confirmBtn.setStylePrimaryName("confirmButton");
+		
+		cancelBtn.addClickhandler(new CancelClickHandler());
+		confirmBtn.addClickHandler(new ConfirmClickHandler());
+		
+		buttonPanel.add(confirmBtn);
+		buttonPanel.add(cancelBtn);
+		
+		
+		contentPanel.add(confirmBtn);
+		contentPanel.add(cancelBtn);
+		contentPanel.add(confirmLabel);
+		contentPanel.add(buttonPanel);
+		
+		this.add(contentPanel);
+		this.center();
+		
+	}
+	
+	public Group getSelectedGroup(){
+		return selectedGroup;
+		
+	}
+	
+	public void setSelectedGroup(Group selectedGroup){
+		this.selectedGroup = selectedGroup;
+		
+	}
+	
+	//Anpassung in TreeViewModel fehlt noch!!!
+}
