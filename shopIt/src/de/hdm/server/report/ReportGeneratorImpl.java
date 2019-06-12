8026f4c76457605f0b194bc115f37a980949816d
Larisa Ciupe <lc030@hdm-stuttgart.de>
2019-06-12 22:40:44 +0200

    Methode für häufig einkaufte Artikel in einem Zeitraum von einem Händler

--- shopIt/src/de/hdm/server/report/ReportGeneratorImpl.java
+++ shopIt/src/de/hdm/server/report/ReportGeneratorImpl.java
@@ -1,16 +1,22 @@
 package de.hdm.server.report;
+import java.util.Vector;
+
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 import de.hdm.shared.ReportGenerator;
+import de.hdm.shared.report.Column;
 import de.hdm.shared.report.CompositeParagraph;
 import de.hdm.shared.report.Row;
 import de.hdm.shared.report.SimpleParagraph;
+import de.hdm.shared.bo.*;
+import de.hdm.server.db.*;
+import de.hdm.server.*;
 
 @SuppressWarnings("serial")//UnterdrÃ¼ckung von Warnungen bezÃ¼glich fehlendem Feld 'serialVersionUID' fÃ¼r eine serialisierbare Klasse
 public class ReportGeneratorImpl extends RemoteServiceServlet implements ReportGenerator {
 	
-	/**Diese Methode soll eine Statistik über häufig einkaufte Artikel in einem Zeitraum
-	 * von einem Händler anzeigen.
+	/**Diese Methode soll eine Statistik über häufig einkaufte Artikel in einem Zeitraum 
+	 * (falls angegeben) von einem Händler anzeigen.
 	 * @Larisa
 	 */
 	
@@ -29,7 +35,7 @@ public class ReportGeneratorImpl extends RemoteServiceServlet implements ReportG
 			/**Zusammenstellung der Kopfdaten (das, was oben auf dem Report steht).
 			 * Die Kopfdaten sind mehrzeilig, deswegen wird die Klasse CompositeParagraph verwendet.
 			 * 
-			 * @author in Anlehnung Thies
+			 * @author Larisa in Anlehnung Thies
 			 */
 			CompositeParagraph header = new CompositeParagraph(); 
 			
@@ -40,10 +46,46 @@ public class ReportGeneratorImpl extends RemoteServiceServlet implements ReportG
 			result.setHeaderData(header); 
 			
 			//Erstellen und Abrufen der benötigten Ergebnisvektoren mittels ShopITAdministration 
-			Vector<Article> articles = this.getAllArticlesForShopWithTime(a, firstDate, lastDate); 
+			Vector<Article> articles = this.aMapper.getAllArticlesForShopWithTime(a, firstDate, lastDate); 
 			
 			//Kopfzeile für die Händlerstatistik-Tabelle. 
 			Row headline = new Row(); 
+			
+			/**
+			 * Die Tabelle wird Zeilen mit 3 Spalten haben. Die erste Spalten entählt
+			 * der Name des Artikels, die zweite die Anzahl des Artikels und die dritte
+			 * Spalte den gewßünschten Zeitraum, falls angegeben. 
+			 * In der Kopfzeile werden die entsprechenden Überschriften angelegt. 
+			 * 
+			 * @author Larisa in Anlehnung Thies
+			 */
+			
+			headline.addColumn(new Column("Article"));
+			headline.addColumn(new Column("Article Quantity"));
+			headline.addColumn(new Column("Period of Time"));
+			
+			//Hinzufügen der Kopfzeile.
+			result.addRow(headline); 
+			
+			//Eine leere Zeile anlegen.
+			Row row = new Row(); 
+			
+			//Die erste Spalte: Artikelname 
+			row.addColumn(new Column(a.getAllArticlesByShop()));
+			row.addColumn(new Column(articles.size() + ""));
+			
+			//Die Zeilen dem Report hinzufügen
+			result.addRow(row); 
+			
+			//Impressum hinzufügen
+			result.addImprint(result); 
+			
+			//Report zurückgeben 
+			return result;
+
+		} else {
+			
+			return null; 
 		}
 	}
 
