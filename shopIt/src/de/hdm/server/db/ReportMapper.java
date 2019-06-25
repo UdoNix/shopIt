dee122d36f5fc50242d95a608f262660fe52c7b8
UdoNix <udo.nix@gmail.com>
2019-06-25 22:41:32 +0200

    ReportMapper, new SQL dump

---
+++ shopIt/src/de/hdm/server/db/ReportMapper.java
@@ -0,0 +1,51 @@
+package de.hdm.server.db;
+
+import java.sql.Timestamp;
+import java.util.Vector;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ReportMapper {
+	
+	public static ReportMapper reportMapper = null;
+	
+	protected ReportMapper() {
+		
+	}
+
+	public static ReportMapper reportMaooer() {
+		if (reportMapper == null) {
+			reportMapper = new ReportMapper();
+		}
+		return reportMapper;
+	}
+	
+	public Vector<ReportObject> createTeamTimeShopReport(int teamId, Timestamp startDate, Timestamp endDate, int shopId) {
+		
+		Connection con = DBConnection.connection();
+		Vector <ReportObject> result = new Vector<ReportObject>();
+		
+		try {
+			Statement stmt = con.createStatement();
+			
+			ResultSet rs = stmt.executeQuery("SELECT COUNT(item.id) AS 'count', article.name AS 'name', unit.quantity AS 'quantity', unit.unit AS 'unit', item.changeDate AS 'changeDate', responsibility.shopId AS 'shopId' "
+					+ "FROM item JOIN article ON item.articleId = article.id JOIN unit ON item.unitId = unit.id JOIN team ON item.teamId = team.id JOIN responsibility ON item.id = responsibility.itemId "
+					+ "WHERE item.teamId = " + teamId + "AND item.changeDate BETWEEN '"+ startDate +"' AND '" + endDate + "' AND shopId = " + shopId + " "
+							+ "GROUP BY item.id ORDER BY COUNT(item.articleId) DESC");
+			
+			while(rs.next()) {
+				ReportObject r = new ReportObject();
+				
+			}
+			
+			
+		} catch (SQLException e){
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+}
