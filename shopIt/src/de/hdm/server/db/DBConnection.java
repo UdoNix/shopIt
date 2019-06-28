611f8f9e5874daa630544394485fb906a80e529b
UdoNix <udo.nix@gmail.com>
2019-06-28 19:04:44 +0200

    google connection reenabled

--- shopIt/src/de/hdm/server/db/DBConnection.java
+++ shopIt/src/de/hdm/server/db/DBConnection.java
@@ -5,6 +5,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.logging.Logger;
 
+import com.google.appengine.api.utils.SystemProperty;
+
 import de.hdm.server.ServersideSettings;
 
 
@@ -21,7 +23,7 @@ public class DBConnection {
 
     // Die URL, um die Datenbank anzusprechen
      
-    private static String googleUrl = "";
+    private static String googleUrl = "jdbc:google:mysql://shopit-241614:europe-west1:shopit?user=demo&password=demo";
     private static String localUrl = "jdbc:mysql://127.0.0.1:8889/shopit?characterEncoding=UTF-8&user=root&password=root";
 
    // statische Methode --> Aufrufbr durch
@@ -35,15 +37,15 @@ public class DBConnection {
         if (con == null) {
             String url = null;
             try {
-//                if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Production) {
-//                    
-//                    Class.forName("");
-//                    url = googleUrl;
-//                } else {
+                if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Production) {
+                    
+                    Class.forName("cmysql.jdbc.GoogleDriver");
+                    url = googleUrl;
+                } else {
                     // Local MySQL instance to use during development.
                     Class.forName("com.mysql.jdbc.Driver");
                     url = localUrl;
-//                }
+                }
                 
                  // Erst dann kann uns der DriverManager eine Verbindung mit den
                  //oben in der Variable url angegebenen Verbindungsinformationen
