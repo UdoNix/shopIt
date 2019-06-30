efe00d8ad7f5e056482f83b0b9c2698af3975624
dibasegmen <ds173@hdm-stuttgart.de>
2019-06-30 21:45:57 +0200

    ShopView Tabelle angelegt

--- shopIt/src/de/hdm/client/gui/ShopView.java
+++ shopIt/src/de/hdm/client/gui/ShopView.java
@@ -1,7 +1,14 @@
 package de.hdm.client.gui;
 
+import java.util.Vector;
+
+import com.google.gwt.cell.client.ButtonCell;
+import com.google.gwt.cell.client.FieldUpdater;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.Column;
+import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
@@ -27,7 +34,7 @@ public class ShopView extends VerticalPanel {
 	}
 
 	Shop shopToDisplay = null;
-	//Widgets, deren Inhalte variable sind, werden als Attribute angelegt.
+	// Widgets, deren Inhalte variable sind, werden als Attribute angelegt.
 
 	TextBox nameTextBox = new TextBox();
 	TextBox postalCodeTextBox = new TextBox();
@@ -38,11 +45,13 @@ public class ShopView extends VerticalPanel {
 	Button newButton = new Button("Als neuen Shop speichern");
 	Button saveButton = new Button("Speichern");
 
-	public void onLoad() {
-		
-		//Beim Anzeigen werden die Widgets z.T. erzeugt. Alle werden in einem
-		 //Raster angeordnet, dessen Größe sich aus dem Platzbedarf der enthaltenen
-		 // Widgets bestimmt.
+	private final  AsyncCallback<Vector<Shop>> getAllCallback;
+
+	public ShopView() {
+
+		// Beim Anzeigen werden die Widgets z.T. erzeugt. Alle werden in einem
+		// Raster angeordnet, dessen Größe sich aus dem Platzbedarf der enthaltenen
+		// Widgets bestimmt.
 
 		Grid shopGrid = new Grid(7, 2);
 		this.add(shopGrid);
@@ -79,10 +88,74 @@ public class ShopView extends VerticalPanel {
 		deleteButton.setEnabled(true);
 		shopGrid.setWidget(6, 1, deleteButton);
 
+		final CellTable<Shop> cellTable = new CellTable<Shop>();
+		getAllCallback = new AsyncCallback<Vector<Shop>>() {
+			@Override
+			public void onFailure(Throwable caught) {
+				Window.alert("Fehler");
+			}
+
+			@Override
+			public void onSuccess(Vector<Shop> result) {
+				cellTable.setRowData(result);
+			}
+		};
+		TextColumn<Shop> idColumn = new TextColumn<Shop>() {
+			@Override
+			public String getValue(Shop object) {
+				return object.getId() + "";
+			}
+		};
+		TextColumn<Shop> nameColumn = new TextColumn<Shop>() {
+			@Override
+			public String getValue(Shop object) {
+				return object.getName();
+			}
+		};
+		TextColumn<Shop> postalCodeColumn = new TextColumn<Shop>() {
+			@Override
+			public String getValue(Shop object) {
+				return object.getPostalCode();
+			}
+		};
+		TextColumn<Shop> streetColumn = new TextColumn<Shop>() {
+			@Override
+			public String getValue(Shop object) {
+				return object.getStreet();
+			}
+		};
+		TextColumn<Shop> cityColumn = new TextColumn<Shop>() {
+			@Override
+			public String getValue(Shop object) {
+				return object.getCity();
+			}
+		};
+		Column<Shop, String> editColumn = new Column<Shop, String>(new ButtonCell()) {
+			@Override
+			public String getValue(Shop object) {
+				return "Bearbeiten";
+			}
+		};
+		editColumn.setFieldUpdater(new FieldUpdater<Shop, String>() {
+			@Override
+			public void update(int index, Shop object, String value) {
+				setSelectedShop(object);
+			}
+		});
+		
+		cellTable.addColumn(idColumn, "Id");
+		cellTable.addColumn(nameColumn, "Name");
+		cellTable.addColumn(postalCodeColumn, "PLZ");
+		cellTable.addColumn(streetColumn, "Straße");
+		cellTable.addColumn(cityColumn, "Stadt");
+		cellTable.addColumn(editColumn, "");
+		add(cellTable);
+		
+		listenVerwaltung.getAllShops(getAllCallback);
 	}
-	
-	//Click handlers und abhängige AsyncCallback Klassen.
-	
+
+	// Click handlers und abhängige AsyncCallback Klassen.
+
 	// ClickHandler zum Löschen eines Shops
 
 	private class DeleteClickHandler implements ClickHandler {
@@ -112,12 +185,12 @@ public class ShopView extends VerticalPanel {
 		public void onSuccess(Void result) {
 			if (shop != null) {
 				setSelectedShop(null);
-//					CellTreeViewModel.delete(shop);
+				listenVerwaltung.getAllShops(getAllCallback);
 			}
 		}
 	}
 
-	//Clickhandler zum Speichern eines Shops
+	// Clickhandler zum Speichern eines Shops
 	private class ChangeClickHandler implements ClickHandler {
 
 		public void onClick(ClickEvent event) {
@@ -142,12 +215,11 @@ public class ShopView extends VerticalPanel {
 		}
 
 		public void onSuccess(Void result) {
-			// Die Änderung wird zum Kunden- und Kontenbaum propagiert.
-//				CellTreeViewModel.updateShop(shopToDisplay);
+			listenVerwaltung.getAllShops(getAllCallback);
 		}
 	}
-	
-	//ClickHandler für einen neuen Shop
+
+	// ClickHandler für einen neuen Shop
 	private class NewClickHandler implements ClickHandler {
 
 		public void onClick(ClickEvent event) {
@@ -168,8 +240,8 @@ public class ShopView extends VerticalPanel {
 			public void onSuccess(Shop shop) {
 				Window.alert("Success");
 				if (shop != null) {
-						setSelectedShop(shop);
-//						CellTreeViewModel.addShop(shop);
+					setSelectedShop(shop);
+					listenVerwaltung.getAllShops(getAllCallback);
 				}
 			}
 		}
