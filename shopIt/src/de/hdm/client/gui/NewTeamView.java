1fe1a88cc0f98ecf05b4446601a5d7631f0dfa03
dibasegmen <ds173@hdm-stuttgart.de>
2019-07-05 15:44:52 +0200

    Verhindern, dass Team ohne Name erstellt wird

--- shopIt/src/de/hdm/client/gui/NewTeamView.java
+++ shopIt/src/de/hdm/client/gui/NewTeamView.java
@@ -17,44 +17,49 @@ import de.hdm.shared.bo.Team;
 public class NewTeamView extends VerticalPanel {
 
 	private ShopITAdministrationAsync listenVerwaltung = ClientsideSettings.getShopItAdministration();
-	
+
 	public NewTeamView(final Tree tree) {
 
 		final TextBox nameTextBox = new TextBox();
-		
+
 		Grid grid = new Grid(2, 2);
-		
+
 		grid.setWidget(0, 0, new Label("Name"));
 		grid.setWidget(0, 1, nameTextBox);
-	
+
 		Button button = new Button("Anlegen");
 		grid.setWidget(1, 1, button);
-		
+
 		add(grid);
-		
+
 		button.addClickHandler(new ClickHandler() {
-			
+
 			@Override
 			public void onClick(ClickEvent event) {
-				listenVerwaltung.createTeam(nameTextBox.getValue(), new AsyncCallback<Team>() {
-					
-					@Override
-					public void onSuccess(Team result) {
-						tree.getRootTreeNode().setChildOpen(1, false);
-						tree.getRootTreeNode().setChildOpen(1, true);
-						
-						nameTextBox.setValue("");
-						
-						Window.alert("Success");
-					}
-					
-					@Override
-					public void onFailure(Throwable caught) {
-						Window.alert("Fehler");
-					}
-				});
+				String name = nameTextBox.getValue();
+				if (name == null || name.isEmpty()) {
+					Window.alert("Bitte Name angeben");
+				} else {
+					listenVerwaltung.createTeam(name, new AsyncCallback<Team>() {
+
+						@Override
+						public void onSuccess(Team result) {
+							tree.getRootTreeNode().setChildOpen(1, false);
+							tree.getRootTreeNode().setChildOpen(1, true);
+
+							nameTextBox.setValue("");
+
+							Window.alert("Success");
+						}
+
+						@Override
+						public void onFailure(Throwable caught) {
+							Window.alert("Fehler");
+						}
+					});
+				}
 			}
 		});
 	}
-	
+
 }
